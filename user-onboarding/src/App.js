262fc01cdd00112dab69c288f3a2d424c56d70cd
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2021-11-10 14:41:44 -0800

    Form values now correctly update; still need: form validation/error messaging, POST requests, display data

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,13 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form';
+import React, { useState } from 'react';
+
+const initialFormValues = {
+  name: '',
+  email: '',
+  password: '',
+  agreeToTerms: false,
+}
+const initialFormErrors = {
+  name: '',
+  email: '',
+  password: '',
+  agreeToTerms: false
+}
+const initialUsers = [];
+const initialDisabled = true;
 
 function App() {
+  const [users, setUsers] = useState(initialUsers)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(initialDisabled)
+
+  const inputChange = (name, value) => {
+    setFormValues({ ...formValues, [name]: value });
+  }
+
+  const formSubmit = () => {
+    const newUser = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password
+    }
+    // postNewUser(newUser);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Create a New User</h1>
-        <Form />
+        <Form 
+          values={formValues}
+          change={inputChange}
+          submit={formSubmit}
+          disabled={disabled}
+          errors={formErrors}
+        />
       </header>
     </div>
   );
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,8 +1,73 @@
 import React from "react";
 
-const Form = () => {
+const Form = (props) => {
+    const { 
+        values, 
+        submit, 
+        change, 
+        disabled, 
+        errors 
+    } = props;
+
+    const onSubmit = evt => {
+        evt.preventDefault()
+        submit()
+    }
+    
+    const onChange = evt => {
+        const { name, value, checked, type } = evt.target
+        const realValue = type === 'checkbox' ? checked : value;
+        change(name, realValue)
+    }
+    
+
     return (
-        <div>
+        <div className='form-container' onSubmit={onSubmit}>
+            <form>
+                <div className='error-container'>
+                    <div className='errors'>
+                        {/* <div>{errors.name}</div>
+                        <div>{errors.email}</div>
+                        <div>{errors.password}</div>
+                        <div>{errors.agreeToTerms}</div> */}
+                    </div>
+                </div>
+                <div className='inputs-container'>
+                    <label>Name
+                        <input 
+                            value={values.name}
+                            onChange={onChange}
+                            name='name'
+                            type='text'
+                        />
+                    </label>
+                    <label>Email
+                        <input 
+                            value={values.email}
+                            onChange={onChange}
+                            name='email'
+                            type='email'
+                        />
+                    </label>
+                    <label>Password
+                        <input 
+                            value={values.password}
+                            onChange={onChange}
+                            name='password'
+                            type='password'
+                        />
+                    </label>
+                    <label>Terms of Service
+                        <input 
+                            onChange={onChange}
+                            name='agreeToTerms'
+                            type='checkbox'
+                            checked={values.agreeToTerms}
+                        />
+                    </label>
+                </div>
+                <button className='submit-button' disabled={disabled}>submit</button>
+            </form>
         </div>
     );
 }
