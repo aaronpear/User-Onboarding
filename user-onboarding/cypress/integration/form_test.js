07e5b6f09c4bf42966db0104c671622ff59b162d
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2021-11-11 11:33:43 -0800

    Created all text input tests

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -4,11 +4,27 @@ describe('Create User App', () => {
     })
     
     const nameInput = () => cy.get('input[name=name]');
+    const emailInput = () => cy.get('input[name=email]');
+    const passInput = () => cy.get('input[name=password]');
 
     it('checks if name input correctly updates', () => {
         nameInput().should('exist');
         nameInput().should('have.value', '');
         nameInput().type('wasd');
         nameInput().should('have.value', 'wasd');
+    })
+
+    it('checks if email input correctly updates', () => {
+        emailInput().should('exist');
+        emailInput().should('have.value', '');
+        emailInput().type('verycoolemailname@ohboy.com');
+        emailInput().should('have.value', 'verycoolemailname@ohboy.com');
+    })
+
+    it('checks if password input correctly updates', () => {
+        passInput().should('exist');
+        passInput().should('have.value', '');
+        passInput().type('atleast7characterslong');
+        passInput().should('have.value', 'atleast7characterslong');
     })
 })
