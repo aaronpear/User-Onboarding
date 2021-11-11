775ce6e2929b427271fd90daf1f397babd9b35d9
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2021-11-11 12:03:43 -0800

    Added full form completion, submission, and render test for single new user

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -7,34 +7,49 @@ describe('Create User App', () => {
     const emailInput = () => cy.get('input[name=email]');
     const passInput = () => cy.get('input[name=password]');
     const termsCheckbox = () => cy.get('input[name=agreeToTerms]');
+    const submitButton = () => cy.get('.submit-button');
 
-    it('checks if name input correctly updates', () => {
+    it('name input correctly updates', () => {
         nameInput().should('exist');
         nameInput().should('have.value', '');
         nameInput().type('wasd');
         nameInput().should('have.value', 'wasd');
     })
 
-    it('checks if email input correctly updates', () => {
+    it('email input correctly updates', () => {
         emailInput().should('exist');
         emailInput().should('have.value', '');
         emailInput().type('verycoolemailname@ohboy.com');
         emailInput().should('have.value', 'verycoolemailname@ohboy.com');
     })
 
-    it('checks if password input correctly updates', () => {
+    it('password input correctly updates', () => {
         passInput().should('exist');
         passInput().should('have.value', '');
         passInput().type('atleast7characterslong');
         passInput().should('have.value', 'atleast7characterslong');
     })
 
-    it('checks if the terms of service checkbox can be checked', () => {
+    it('the terms of service checkbox can be checked', () => {
         termsCheckbox().should('exist');
         termsCheckbox().should('not.be.checked');
         termsCheckbox().click();
         termsCheckbox().should('be.checked');
-        termsCheckbox().click();
-        termsCheckbox().should('not.be.checked');
+    })
+
+
+    describe('checks for complete form completion and submission', () => {
+        
+        it('fills all fields and submits the form; new user successfully rendered', () => {
+            nameInput().type('firstname lastname');
+            emailInput().type('thesickestemail@googmeal.com');
+            passInput().type('1234567');
+            termsCheckbox().click();
+            submitButton().should('exist');
+            submitButton().click();
+
+            cy.contains('firstname lastname');
+            cy.contains('thesickestemail@googmeal.com');
+        })
     })
 })
